5c500674f30474f46cdcbc85b7fd1f6204bf5afb
GrayOtter <42044381+GrayOtter@users.noreply.github.com>
2019-08-02 09:26:25 -0700

    hopefully easier to read

---
+++ para.js
@@ -0,0 +1,6 @@
+//I choose to write about ekelokorpi/panda-engine under JavaScript Game Engines. 
+//From what I can see the way they deal with pull request is only those with write access to the repository could merge pull request. 
+//They properly make things added or found as “features” or “Bugs” with good description of the problems that are new or ongoing. 
+//They have a code of conduct for how people should act and contribute. 
+//They also have a Contributing guide. 
+//They properly number updates/releases and keep the whole project well organized.
